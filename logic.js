1e4ae6289b8abbc254d51b507e95e1f7b87aef1c
Omar Abou Nassif Mourad <omar.nassif98@gmail.com>
2026-02-01 04:21:45 +0200

    Rewrite

---
+++ logic.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const aiButton = document.getElementById('aiButton');
+  if (!aiButton) return;
+
+  aiButton.addEventListener('click', () => {
+    alert('If you really expected a chatbot, re-evaluate your life choices');
+    window.location.href = "/files/haha-you-look-hilarious.jpg"
+    });
+aiButton.style.display = 'block';
+});
