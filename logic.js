1b4a84a36e53bf9d90cc260d331cd0309042cd34
Omar Abou Nassif Mourad <omar.nassif98@gmail.com>
2026-02-12 15:28:14 +0000

    Return of AI mode

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
+  if(!urlParams.has('ai_mode')) return;
   const aiButton = document.getElementById('aiButton');
   if (!aiButton) return;
-
   aiButton.addEventListener('click', () => {
     alert('If you really expected to talk to a chatbot to simplify a website like this even further, re-evaluate your life choices');
     window.location.href = "/files/haha-you-look-hilarious.jpg"
