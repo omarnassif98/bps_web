7049e8dba7985fadb5b04ae78c0612feeaa35415
Omar Abou Nassif Mourad <omar.nassif98@gmail.com>
2026-06-27 14:10:38 +0300

    Booklab

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import socket
+from zeroconf import ServiceInfo, Zeroconf
 from flask import Flask, send_from_directory, abort
 import os
 
@@ -24,5 +26,31 @@ def clean_route(slug):
 
     return abort(404)
 
+
+def start_mdns():
+    # Discover local IP
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+
+    info = ServiceInfo(
+        "_http._tcp.local.",
+        "flaskserver._http._tcp.local.",
+        addresses=[socket.inet_aton(ip)],
+        port=5000,
+        server="omar.local.",
+    )
+
+    zc = Zeroconf()
+    zc.register_service(info)
+    print(f"Server available at http://omar.local:8000")
+    return zc, info
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True, port=8000)
+    zc, info = start_mdns()
+    try:
+        app.run(host='0.0.0.0', port=8000)
+    finally:
+        zc.unregister_service(info)
+        zc.close()
